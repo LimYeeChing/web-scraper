14ac8d597e8eea51b3f156c2dad1657b7ac09dae
alistair-fang <alistair.fang@n-actuarial.com>
2024-05-05 23:02:38 +0800

    added code and results of hla.csv

---
+++ HLA.R
@@ -0,0 +1,33 @@
+library(rvest)
+
+OUTPUT_FILE_PATH <- "results/HLA.csv"
+
+# HLA's website currently shows all of its personal products on one page 
+
+PRODUCTS_PAGE <- "https://www.hla.com.my/en/personal.html"
+
+results <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(results) <- c("product_type", "product_name", "product_description")
+
+html <- read_html_live(PRODUCTS_PAGE)
+html$view()
+product_type <-
+  html %>%
+  html_elements(".mb-1.text-secondary-blue-base") %>%
+  html_text2()
+
+product_name <- 
+  html %>%
+  html_elements(".heading-h5-bold.mb-4.text-neutral-black-base") %>%
+  html_text2()
+
+product_description <-
+  html %>%
+  html_elements(".flex-grow.px-6.pb-6.pt-5") %>%
+  html_text2() 
+
+results <- data.frame(product_type, product_name, product_description) 
+
+# Write results to .csv file
+
+write.csv(results, file = OUTPUT_FILE_PATH, row.names = FALSE)
