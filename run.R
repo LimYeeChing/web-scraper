5377f34c30de7b239577a6adf21f89b45d846b06
alistair-fang <alistair.fang@n-actuarial.com>
2024-05-10 18:04:24 +0800

    Cleaned up code, added run.R which runs all the code and compiles the results into a single file

---
+++ run.R
@@ -0,0 +1,39 @@
+FILE_PATH_OUTPUT <- "results/compilation.csv"
+
+COMPANY_NAMES <- c("AIA" = "aia", 
+                   "Allianz" = "allianz",
+                   "AmMetLife" = "ammetlife",
+                   "Etiqa" = "etiqa",
+                   "FWD" = "fwd",
+                   "Great Eastern" = "ge",
+                   "Generali" = "generali",
+                   "Hong Leong Assurance" = "hla",
+                   "Manulife" = "manulife",
+                   "MCIS" = "mcis",
+                   "Prudential" = "prudential",
+                   "Sun Life" = "sunlife",
+                   "Tokio Marine" = "tokiomarine",
+                   "Zurich" = "zurich")
+
+code_file_names <- paste0(COMPANY_NAMES, ".R")
+
+# Load all results in separate CSVs in the "Results" folder
+# so they can be checked/retrieved individually 
+
+for (file_name in code_file_names) {source(file_name)}
+
+# Compile all results into one file ----
+
+results_file_names <- paste0("results/", COMPANY_NAMES, ".csv")
+
+results <- data.frame(matrix(nrow = 0, ncol = 4))
+colnames(results) <- c("company", "product_type", "product_name", "product_description")
+
+for (i in 1:length(COMPANY_NAMES)){
+
+  company_results <- cbind(company = names(COMPANY_NAMES[i]), read.csv(results_file_names[i]))
+  results <- rbind(results, company_results)
+
+}
+
+write.csv(results, file = FILE_PATH_OUTPUT, row.names = FALSE)
